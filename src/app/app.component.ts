691cebe514b097bb6475deabbb5789431907c979
14DAVEN18 <davinci1996@live.com>
2018-02-21 18:39:09 -0500

    Changes - Menu edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -92,11 +92,20 @@ export class AppComponent {
     });
   }
 
-  select(item) {
-    this.selected = item; 
-  };
+  // select(item) {
+  //   this.selected = item; 
+  // };
 
-  isActive(item) {
-    return this.selected === item;
-  };
+  // isActive(item) {
+  //   return this.selected === item;
+  // };
+
+  function() {
+    $('.unordered li accordion').click(function(e) {
+      e.preventDefault();
+      var $this = $(this);
+      $this.closest('ul').children('li').removeClass('active');
+      $this.parent().addClass('active');
+    });
+  }
 }
