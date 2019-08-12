e6a9fb7e984c554cdd0298df8a25ae2895e2c9e0
Jhon Gabriel Castellanos Jimenez <corio27@gmail.com>
2019-08-12 18:25:51 +0000

    actualización ambiente de desarrollo
    actualización ambiente de desarrollo

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://10.20.0.254/apoyo_alimentario_cliente/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
